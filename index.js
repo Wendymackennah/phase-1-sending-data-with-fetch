fe2e2c0005df06d30568b239ba42c6b4e5f8903a
wendymackennah <wendymackennah5@gmail.com>
2024-01-15 14:33:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  const configObject = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch("http://localhost:3000/users", configObject)
+    .then(response => response.json())
+    .then(data => {
+      document.body.innerHTML = data.id;
+    })
+    .catch(error => {
+      document.body.innerHTML = error.message;
+    });
+}
+
+// Test the function
+submitData();
+
